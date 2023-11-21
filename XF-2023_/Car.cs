c58090da72b556f4cfa55bfc5eb6c4dd5e526a7b
denisacraita <denisa.craita@cncn.ro>
2023-11-21 18:58:32 +0200

    Add project files.

---
+++ XF-2023_/Car.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XF_2023_
+{
+    public class Car
+    {
+        public string color;
+        public string name;
+        public int maxSpeed;
+        public int year;
+        public Car(string nameName,string colorName,int yearValue,int maxSpeedValue)
+        {
+            name = nameName;
+            color = colorName;
+            year = yearValue;
+            maxSpeed = maxSpeedValue;
+        }
+        public Car(string nameName)
+        {
+            name = nameName;
+        }
+        public void fullThrottle()
+        {
+            Console.WriteLine(name + " IS GOING AT THE SPEED OF " + maxSpeed);
+        }
+    }
+}
---
+++ XF-2023_/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XF_2023_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car Ford = new Car("Mustang", "white", 2021, 100);
+            Car Opel = new Car("Astra", "blue", 2020, 120);
+            Car Volvo = new Car("Xc-98");
+
+            Console.WriteLine(Ford.name);
+            Console.WriteLine(Ford.color);
+            Console.WriteLine(Ford.year);
+            Ford.fullThrottle();
+            Console.WriteLine(Opel.name);
+            Console.WriteLine(Opel.color);
+            Console.WriteLine(Opel.year);
+            Opel.fullThrottle();
+            Console.ReadKey();
+        }
+    }
+}
